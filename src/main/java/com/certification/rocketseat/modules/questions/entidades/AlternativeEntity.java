0c98f36cda2e939ae0b4fdf3cfade9b4f6079f37
Fabricio Vieira <bvieira.fabricio@gmail.com>
2024-02-08 00:22:07 -0300

    Criação das  classes AlternativeEntity, AlternativesResultDto e AnswersCertificationsEntity.

---
+++ src/main/java/com/certification/rocketseat/modules/questions/entidades/AlternativeEntity.java
@@ -0,0 +1,28 @@
+package com.certification.rocketseat.modules.questions.entidades;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "alternatives")
+public class AlternativeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String description;
+
+    private boolean isCorrect;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
