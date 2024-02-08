2f5e726c79f0c2ebc3e9d8d1ecb5052996c16bd5
Fabricio Vieira <bvieira.fabricio@gmail.com>
2024-02-08 00:17:18 -0300

    alterações nas classes de StudentController e StudentEntity

--- src/main/java/com/certification/rocketseat/modules/student/controllers/StudentController.java
+++ src/main/java/com/certification/rocketseat/modules/student/controllers/StudentController.java
@@ -1,6 +1,8 @@
 package com.certification.rocketseat.modules.student.controllers;
 
 import com.certification.rocketseat.modules.student.dto.VerifyHasCertificationDto;
+import com.certification.rocketseat.modules.student.usecases.VerifyIfHasCertificationUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/students")
 public class StudentController {
 
+    @Autowired
+    private VerifyIfHasCertificationUseCase verifyIfHasCertificationUseCase;
+
     @PostMapping("/verifyifhascertification")
     public String verifyIfHasCertification(@RequestBody VerifyHasCertificationDto verifyHasCertificationDto) {
         //email
         //tecnologia
-
-        System.out.println(verifyHasCertificationDto);
+        var result = verifyIfHasCertificationUseCase.execute(verifyHasCertificationDto);
+        if (result) {
+            return "Usuário já fez a prova!";
+        }
         return "O aluno pode fazer a prova!";
     }
 }
