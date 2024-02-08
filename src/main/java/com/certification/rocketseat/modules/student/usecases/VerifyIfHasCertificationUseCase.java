c20c3f27b1f113327a3f2135d76709e687342902
Fabricio Vieira <bvieira.fabricio@gmail.com>
2024-02-08 00:18:04 -0300

    alteração na classe VerifyIfHasCertificationUseCase

---
+++ src/main/java/com/certification/rocketseat/modules/student/usecases/VerifyIfHasCertificationUseCase.java
@@ -0,0 +1,20 @@
+package com.certification.rocketseat.modules.student.usecases;
+
+import com.certification.rocketseat.modules.student.dto.VerifyHasCertificationDto;
+import com.certification.rocketseat.modules.student.repository.CertificationStudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VerifyIfHasCertificationUseCase {
+    @Autowired
+    private CertificationStudentRepository repository;
+
+    public boolean execute(VerifyHasCertificationDto dto) {
+        var result = this.repository.findStudentEmailAndTecnology(dto.getEmail(), dto.getTecnology());
+        if (!result.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+}
