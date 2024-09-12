90d1ee3e5bff772698fe09dd8acecd7923ec84c3
fabriciovieira <Bibito23@>
2024-09-12 20:50:24 -0300

    alterações no dto da classe student

--- src/main/java/com/certification/rocketseat/modules/student/dto/StudentCertificationAnswerDto.java
+++ src/main/java/com/certification/rocketseat/modules/student/dto/StudentCertificationAnswerDto.java
@@ -14,4 +14,36 @@ public class StudentCertificationAnswerDto {
     private String technology;
     private String name;
     private List<QuestionAnswerDto> questionAnswerDto;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTechnology() {
+        return technology;
+    }
+
+    public void setTechnology(String technology) {
+        this.technology = technology;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<QuestionAnswerDto> getQuestionAnswerDto() {
+        return questionAnswerDto;
+    }
+
+    public void setQuestionAnswerDto(List<QuestionAnswerDto> questionAnswerDto) {
+        this.questionAnswerDto = questionAnswerDto;
+    }
 }
