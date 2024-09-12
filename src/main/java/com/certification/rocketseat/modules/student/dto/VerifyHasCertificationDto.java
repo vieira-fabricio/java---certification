2a58b626307dc6574830ffc79b40cff1a42ac0c3
fabriciovieira <Bibito23@>
2024-09-12 20:51:37 -0300

    alterações no dto da entidade student

--- src/main/java/com/certification/rocketseat/modules/student/dto/VerifyHasCertificationDto.java
+++ src/main/java/com/certification/rocketseat/modules/student/dto/VerifyHasCertificationDto.java
@@ -12,4 +12,28 @@ public class VerifyHasCertificationDto {
     private String email;
     private String name;
     private String technology;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTechnology() {
+        return technology;
+    }
+
+    public void setTechnology(String technology) {
+        this.technology = technology;
+    }
 }
