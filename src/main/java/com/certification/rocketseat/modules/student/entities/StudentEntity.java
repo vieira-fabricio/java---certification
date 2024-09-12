447476a5910ceb45efc3a27d067185a7a3771848
fabriciovieira <Bibito23@>
2024-09-12 20:54:42 -0300

    alterações na entidade student

--- src/main/java/com/certification/rocketseat/modules/student/entities/StudentEntity.java
+++ src/main/java/com/certification/rocketseat/modules/student/entities/StudentEntity.java
@@ -28,4 +28,59 @@ public class StudentEntity {
     @CreationTimestamp
     private LocalDateTime createdAt;
 
+    public StudentEntity(String email) {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<CertificationStudentEntity> getCertificationStudentEntity() {
+        return certificationStudentEntity;
+    }
+
+    public void setCertificationStudentEntity(List<CertificationStudentEntity> certificationStudentEntity) {
+        this.certificationStudentEntity = certificationStudentEntity;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public static class Builder {
+        private String email;
+
+        public StudentEntity.Builder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public StudentEntity build() {
+            return new StudentEntity(email);
+        }
+    }
 }
