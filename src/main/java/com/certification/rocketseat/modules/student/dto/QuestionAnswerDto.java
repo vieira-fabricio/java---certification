2f42064102beb189b8518308239107d32e612562
fabriciovieira <Bibito23@>
2024-09-12 20:49:37 -0300

    alterações no question answer dto da entidade question

--- src/main/java/com/certification/rocketseat/modules/student/dto/QuestionAnswerDto.java
+++ src/main/java/com/certification/rocketseat/modules/student/dto/QuestionAnswerDto.java
@@ -14,4 +14,28 @@ public class QuestionAnswerDto {
     private UUID questionId;
     private UUID alternativeId;
     private boolean isCorrect;
+
+    public UUID getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(UUID questionId) {
+        this.questionId = questionId;
+    }
+
+    public UUID getAlternativeId() {
+        return alternativeId;
+    }
+
+    public void setAlternativeId(UUID alternativeId) {
+        this.alternativeId = alternativeId;
+    }
+
+    public boolean isCorrect() {
+        return isCorrect;
+    }
+
+    public void setCorrect(boolean correct) {
+        isCorrect = correct;
+    }
 }
