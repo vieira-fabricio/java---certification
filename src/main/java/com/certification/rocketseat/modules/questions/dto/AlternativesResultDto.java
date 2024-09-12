f2c19899f2085348bcf652401f23a0a1f271f6e7
fabriciovieira <Bibito23@>
2024-09-12 20:42:41 -0300

    alteração no dto do projeto

--- src/main/java/com/certification/rocketseat/modules/questions/dto/AlternativesResultDto.java
+++ src/main/java/com/certification/rocketseat/modules/questions/dto/AlternativesResultDto.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
@@ -15,4 +16,7 @@ public class AlternativesResultDto {
 
     private UUID id;
     private String description;
+    private boolean isCorrect;
+    private LocalDateTime createdAt;
+
 }
