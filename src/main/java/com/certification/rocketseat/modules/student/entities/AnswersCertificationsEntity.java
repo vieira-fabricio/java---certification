1d52109bce6821507f662458c3b898d10e89be84
fabriciovieira <Bibito23@>
2024-09-12 20:52:29 -0300

    alterações na entidade answer certification

--- src/main/java/com/certification/rocketseat/modules/student/entities/AnswersCertificationsEntity.java
+++ src/main/java/com/certification/rocketseat/modules/student/entities/AnswersCertificationsEntity.java
@@ -44,4 +44,112 @@ public class AnswersCertificationsEntity {
     private boolean isCorrect;
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    public AnswersCertificationsEntity(UUID questionID, UUID answerID, boolean isCorrect) {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getCertificationID() {
+        return certificationID;
+    }
+
+    public void setCertificationID(UUID certificationID) {
+        this.certificationID = certificationID;
+    }
+
+    public CertificationStudentEntity getCertificationStudentEntity() {
+        return certificationStudentEntity;
+    }
+
+    public void setCertificationStudentEntity(CertificationStudentEntity certificationStudentEntity) {
+        this.certificationStudentEntity = certificationStudentEntity;
+    }
+
+    public UUID getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(UUID studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public StudentEntity getStudentEntity() {
+        return studentEntity;
+    }
+
+    public void setStudentEntity(StudentEntity studentEntity) {
+        this.studentEntity = studentEntity;
+    }
+
+    public UUID getQuestionID() {
+        return questionID;
+    }
+
+    public void setQuestionID(UUID questionID) {
+        this.questionID = questionID;
+    }
+
+    public UUID getAnswerID() {
+        return answerID;
+    }
+
+    public void setAnswerID(UUID answerID) {
+        this.answerID = answerID;
+    }
+
+    public boolean isCorrect() {
+        return isCorrect;
+    }
+
+    public void setCorrect(boolean correct) {
+        isCorrect = correct;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public static class Builder {
+        private UUID questionID;
+        private UUID answerID;
+        public boolean isCorrect;
+
+        public AnswersCertificationsEntity.Builder questionID(UUID questionID) {
+            this.questionID = questionID;
+            return this;
+        }
+
+        public AnswersCertificationsEntity.Builder answerID(UUID answerID) {
+            this.answerID = answerID;
+            return this;
+        }
+
+        public AnswersCertificationsEntity.Builder isCorrect(boolean isCorrect) {
+            this.isCorrect = isCorrect;
+            return this;
+        }
+
+        public AnswersCertificationsEntity build() {
+            return new AnswersCertificationsEntity(questionID, answerID, isCorrect);
+        }
+    }
 }
