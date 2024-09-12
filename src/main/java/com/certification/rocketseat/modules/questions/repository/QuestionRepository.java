943250c9aa8022d8152ba4d7b19235afe4a70174
fabriciovieira <Bibito23@>
2024-09-12 20:46:48 -0300

    alteração no repositório da entidade question

--- src/main/java/com/certification/rocketseat/modules/questions/repository/QuestionRepository.java
+++ src/main/java/com/certification/rocketseat/modules/questions/repository/QuestionRepository.java
@@ -1,12 +1,12 @@
 package com.certification.rocketseat.modules.questions.repository;
 
-import com.certification.rocketseat.modules.questions.entidades.QuestionEntity;
+import com.certification.rocketseat.modules.questions.entidades.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.UUID;
 
-public interface QuestionRepository extends JpaRepository<QuestionEntity, UUID> {
+public interface QuestionRepository extends JpaRepository<Question, UUID> {
 
-    List<QuestionEntity> findByTechnology(String technology);
+    List<Question> findByTechnology(String technology);
 }
